1da95fdcaf6893b2a115c1906006cf7909f7dd2c
hurgleburgler <bubasti@gmail.com>
2014-08-17 09:12:04 -0700

    A good beginning is only half the task.

---
+++ budgetz/api.py
@@ -0,0 +1,10 @@
+from tastypie.resources import ModelResource
+from tastypie import fields
+#from budgetz.models import Foo 
+
+#class Resource(ModelResource):
+#    class Meta:
+#        queryset = Foo.objects.all()
+#        resource_name = 'resource'
+
+
